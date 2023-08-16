9d7044d97898d265d3338f2c92505ece8c36084c
Alex A <alex.andsten@gmail.com>
2023-08-16 20:33:39 +0200

    start

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import HeaderComp from './.expo/components/HeaderComp';
+import FooterComp from './.expo/components/FooterComp';
+import ListaComp from './.expo/components/ListaComp';
+
+import { NavigationContainer } from '@react-navigation/native';
+import Navigation from './.expo/components/Navigation';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+    
+      <HeaderComp/>
+       <Navigation />  
+      <View style={styles.container}>
+     
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      
+      </View>
+      <FooterComp/>
+    </>
   );
 }
 
